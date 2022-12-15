3198c2fc2e2b62e713dbf216653bd9adef015573
somesh9870 <someshrawat106@gmail.com>
2022-12-16 00:02:35 +0530

    day-3 tasks completed by somesh

---
+++ components/footer.js
@@ -0,0 +1,58 @@
+const footer = () => {
+    return `<div class="footer-top">
+    <div>
+      <i class="fa-brands fa-facebook-f"></i>
+      <i class="fa-brands fa-linkedin-in"></i>
+      <i class="fa-brands fa-twitter"></i>
+      <i class="fa-brands fa-instagram"></i>
+    </div>
+    <div class="foot">
+      <div>
+      BY PLACE
+      </div>
+      <p>Internship in Mumbai</p>
+        <p>Internship in Delhi</p>
+        <p>Internship in Banglore</p>
+        <p>Internship in Pune</p>
+        <p>Internship in Hyderabad</p>
+    </div>
+    <div class="foot">
+      <div>
+        BY PROFILE
+      </div>
+        <p>Marketing Internships 
+        <p>Business Development Internships</p>
+        <p>Content Writing Internships</p>
+        <p>Graphic Designing Internships</p>
+        <p>HR Internships</p>
+        <p>Engineering Internships</p>
+      
+    </div>
+    <div class="foot" class="foot-last">
+      <div>BY TYPE</div>
+      
+        <p>Full Time Internships</p>
+        <p>Part Time Internships</p>
+        <p>Work From Home Internship</p>
+      
+    </div>
+  </div>
+
+
+  <div class="footer-bottom">
+    <div>
+      <p>REFUND & CANCELLATION POLICY</p>
+      <p>COURSES</p>
+      <p>TERMS AND CONDITIONS</p>
+      <p>PRIVACY POLICY</p>
+      <p>CONTACT US</p>
+      <p>ABOUT US</p>
+      <p>SITEMAP</p>
+    </div>
+    <div>
+      <p>© 2015 - 2022 INTERNTHEORY. ALL RIGHTS RESERVED.</p>
+    </div>
+  </div>`
+}
+
+export default footer;
